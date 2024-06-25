697ecd3f6b2f5d06d13754a68e1ea04386340268
piyushgautamtg <piyushgautamtg@gmail.com>
2024-06-25 20:19:23 +0530

    new changes

--- js/common.js
+++ js/common.js
@@ -30,7 +30,7 @@
             inner.innerHTML= "";
             var project=[];
             if(type=="all"){project=projects; }
-            else if(type=="apartment" || type=="villa")
+            else if(type=="project" || type=="villa")
                     project=projects.filter((ele)=> ele.type==type);
             else project=projects.filter((ele)=> ele.status==type);
 
@@ -47,45 +47,14 @@
                         <h5 class="mb-1 ms-2 fw-bold">${project[i].name}</h5>
                         <p class="mb-1 ms-2">${project[i].site}</p>
                             <table class="table table-responsive table-striped my-0 ">
+                                <tr><td class="fw-bold">Status:</td><td>${project[i].status}</td></tr>
+                                <tr><td class="fw-bold">Type of work:</td><td>${project[i].work}</td></tr>
                                 <tr><td class="fw-bold">Area:</td><td>${project[i].area}</td></tr>
-                                <tr><td class="fw-bold">No of building:</td><td>${project[i].nobuilding}</td></tr>
-                                <tr><td class="fw-bold">No of units:</td><td>${project[i].nounits}</td></tr>
-                                <tr><td class="fw-bold">No of available units:</td><td>${project[i].nounitsava}</td></tr>
-                                <tr><td class="fw-bold">List Building:</td><td>${project[i].listbuilding}</td></tr>
+
                             </table>
                     </div>
                 </div>`;
             }
     };
 
-    function getAllProjectsArabic(count,type,e){
-        var inner=document.querySelector('#innerProject');
-            inner.innerHTML= "";
-            var project=[];
-            if(type=="all"){project=projectsArabic; }
-            else if(type=="apartment" || type=="villa")
-                    project=projectsArabic.filter((ele)=> ele.type==type);
-            else project=projectsArabic.filter((ele)=> ele.status==type);
-
-            if(count==0)
-                count=projectsArabic.length;
-            for(var i=0;i<count;i++)
-            {
-                inner.innerHTML+=`<div class=" col-md-6 col-lg-4  mt-5 ">
-                    <div class="project">
-                        <a href="project-details.html?pid=${project[i].id}">
-                            <img src="image/${project[i].gallery[0]}" class="project-img mb-2"/>
-                        </a>
-                        <h5 class="mb-1 ms-2 fw-bold">${project[i].name}</h5>
-                        <p class="mb-1 ms-2">${project[i].site}</p>
-                            <table class="table table-responsive table-striped my-0 ">
-                                <tr><td class="fw-bold">المساحة:</td><td>${project[i].area}</td></tr>
-                                <tr><td class="fw-bold">عدد البنايات:</td><td>${project[i].nobuilding}</td></tr>
-                                <tr><td class="fw-bold">عدد الوحدات:</td><td>${project[i].nounits}</td></tr>
-                                <tr><td class="fw-bold">عدد الوحدات المتاحة:</td><td>${project[i].nounitsava}</td></tr>
-                                <tr><td class="fw-bold">نوع البناية:</td><td>${project[i].listbuilding}</td></tr>
-                            </table>
-                    </div>
-                </div>`;
-            }
-    };
+    
